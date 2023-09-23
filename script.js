6393398f5c7dc335b6ceb962bddee76991e64700
Mahdi <mahdimey17@gmail.com>
2023-09-23 23:15:35 +0330

    added mousemove functionality

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ let cursor = document.querySelector('.cursor')
 let cursor2 = document.querySelector('.cursor2')
 
 document.addEventListener('mousemove', e => {
+    
+    cursor.style.display = 'block'
+    cursor2.style.display = 'block'
 
+    cursor.style.left = `${e.clientX}px`
+    cursor.style.top = `${e.clientY}px`
+    cursor2.style.left = `${e.clientX}px`
+    cursor2.style.top = `${e.clientY}px`
 })
 document.addEventListener('mouseleave', () => {
 
